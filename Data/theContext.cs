2cf21245942cdc3c97a084ac95c7ef821e138c13
SeKiZo0 <105446823+SeKiZo0@users.noreply.github.com>
2023-11-19 22:04:14 +0200

    Add project files.

---
+++ Data/theContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class LibraryContext : DbContext
+    {
+        protected readonly IConfiguration _configuration;
+
+        public LibraryContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(_configuration.GetConnectionString("PostgreSQL"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Models
+{
+    public class Book
+    {
+        public string Id { get; set; }  
+        public string Title { get; set; }
+    }
+}
